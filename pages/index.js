731c683d247db1fc00880e5b427f98257cf36101
Martin Garcia <newluxfero@gmail.com>
2019-01-12 18:29:32 +0100

    Fix lint on pages

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,54 @@
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Pagination from '../components/Pagination';
 import PostEntry from '../components/PostEntry';
 import { paginate, getFileInfo } from '../lib';
 import css from './index.module.css';
 
-const App = ({ results, page, total, id }) => (
-  <>
-    <ul className={css.list}>
-      {results.map(props => (
-        <PostEntry key={id} {...props} />
-      ))}
-    </ul>
-    <Pagination total={total} page={page} />
-  </>
-);
-
-App.getInitialProps = async function(context) {
-  // TODO: Refactor
-  const page = parseInt(context.query.page, 10) || 1;
-  const limit = 5;
-  const posts = (ctx => {
-    const files = ctx
-      .keys()
-      .map(f => f.replace('./', '').replace('.md', ''))
-      .sort();
-    const contents = ctx.keys().map(ctx);
-    const results = paginate(files.map((f, i) => [f, contents[i]]).reverse(), page, limit).map(
-      ([f, c]) => getFileInfo(f, c)
-    );
-    return {
-      page,
-      limit,
-      total: Math.ceil(files.length / limit),
-      results
-    };
-  })(require.context('../_posts', true, /\.md$/));
-
-  return {
-    ...posts
+export default class App extends Component {
+  static propTypes = {
+    results: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+    total: PropTypes.number.isRequired,
+    page: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired
   };
-};
 
-App.propTypes = {
-  results: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-  total: PropTypes.number.isRequired,
-  page: PropTypes.number.isRequired,
-  id: PropTypes.string.isRequired
-};
+  static async getInitialProps(context) {
+    // TODO: Refactor
+    const page = parseInt(context.query.page, 10) || 1;
+    const limit = 5;
+    const posts = (ctx => {
+      const files = ctx
+        .keys()
+        .map(f => f.replace('./', '').replace('.md', ''))
+        .sort();
+      const contents = ctx.keys().map(ctx);
+      const results = paginate(files.map((f, i) => [f, contents[i]]).reverse(), page, limit).map(
+        ([f, c]) => getFileInfo(f, c)
+      );
+      return {
+        page,
+        limit,
+        total: Math.ceil(files.length / limit),
+        results
+      };
+    })(require.context('../_posts', true, /\.md$/));
+    return {
+      ...posts
+    };
+  }
 
-export default App;
+  render() {
+    const { results, page, total, id } = this.props;
+    return (
+      <>
+        <ul className={css.list}>
+          {results.map(props => (
+            <PostEntry key={id} {...props} />
+          ))}
+        </ul>
+        <Pagination total={total} page={page} />
+      </>
+    );
+  }
+}
--- pages/post.js
+++ pages/post.js
@@ -1,4 +1,5 @@
 import { withRouter } from 'next/router';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 import matter from 'gray-matter';
 import getReadingTime from 'reading-time';
@@ -10,51 +11,54 @@ import { gistPlugin, remarkReactConfig } from '../lib';
 import SocialShare from '../components/SocialShare';
 import css from './post.module.css';
 
-const Post = ({ content, title, year, month, day, readingTime, slug }) => {
-  const url = `https://magarcia.github.io/${year}/${month}/${day}/${slug}`;
-  return (
-    <>
-      <article className={css.article}>
-        <h1 className={css.title}>{title}</h1>
-        <MetaInfo {...{ year, month, day, readingTime }} />
-        <Content>
-          {
-            remark()
-              .use(gistPlugin)
-              .use(remarkReact, remarkReactConfig)
-              .processSync(content).contents
-          }
-        </Content>
-      </article>
-      <SocialShare title={title} url={url} />
-    </>
-  );
-};
-
-Post.getInitialProps = async function(context) {
-  const { id, year, month, day } = context.query;
-  const post = await import(`../_posts/${year}-${month}-${day}-${id}.md`);
-  const document = matter(post.default);
-
-  return {
-    slug: id,
-    year,
-    month,
-    day,
-    content: document.content,
-    readingTime: getReadingTime(document.content),
-    title: document.data.title
+class Post extends Component {
+  static propTypes = {
+    year: PropTypes.number.isRequired,
+    month: PropTypes.number.isRequired,
+    day: PropTypes.number.isRequired,
+    slug: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    readingTime: PropTypes.string.isRequired
   };
-};
 
-Post.propTypes = {
-  year: PropTypes.number.isRequired,
-  month: PropTypes.number.isRequired,
-  day: PropTypes.number.isRequired,
-  slug: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
-  readingTime: PropTypes.string.isRequired
-};
+  static async getInitialProps(context) {
+    const { id, year, month, day } = context.query;
+    const post = await import(`../_posts/${year}-${month}-${day}-${id}.md`);
+    const document = matter(post.default);
+
+    return {
+      slug: id,
+      year,
+      month,
+      day,
+      content: document.content,
+      readingTime: getReadingTime(document.content),
+      title: document.data.title
+    };
+  }
+
+  render() {
+    const { content, title, year, month, day, readingTime, slug } = this.props;
+    const url = `https://magarcia.github.io/${year}/${month}/${day}/${slug}`;
+    return (
+      <>
+        <article className={css.article}>
+          <h1 className={css.title}>{title}</h1>
+          <MetaInfo {...{ year, month, day, readingTime }} />
+          <Content>
+            {
+              remark()
+                .use(gistPlugin)
+                .use(remarkReact, remarkReactConfig)
+                .processSync(content).contents
+            }
+          </Content>
+        </article>
+        <SocialShare title={title} url={url} />
+      </>
+    );
+  }
+}
 
 export default withRouter(Post);
