c0483c7853e0daf648dcb5065dcbc1e07d17b42d
Martin Garcia <newluxfero@gmail.com>
2019-01-13 11:36:02 +0100

    Fix typo

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0, viewport-fit=contain"
