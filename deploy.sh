f2c62a71986a369cbd51dd2c5b209f1b37f28f43
Martin Garcia <newluxfero@gmail.com>
2019-01-13 14:32:31 +0100

    Update deployment script on CI

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  cd out
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-page https://${GH_TOKEN}@github.com/magarcia/magarcia.github.io.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
+
+setup_git
+commit_website_files
+upload_files
