6885e3d1bd4994f50a5f1f21de2f6250cf303c50
Martin Garcia <newluxfero@gmail.com>
2019-01-28 01:07:56 +0100

    Fix 404 header

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 import lost from '../assets/lost.svg';
 
 export default ({ location }) => (
-  <Layout location={location}>
+  <Layout location={location} title="magarcia">
     <img src={lost} alt="Lost" style={{ width: '45rem', position: 'absolute' }} />
     <h1 style={{ fontSize: '23rem' }}>404</h1>
   </Layout>
