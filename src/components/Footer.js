253c22f8e63f2d37614244bf2f60b1de0a9d4bdc
Martin Garcia <newluxfero@gmail.com>
2020-03-24 18:32:07 +0100

    Update social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,27 +3,19 @@ import React from 'react';
 export default () => (
   <footer className="footer">
     <div>
-      <a href="https://mobile.twitter.com/martinprins" target="_blank" rel="noopener noreferrer">
+      <a href="https://twitter.com/martinprins" target="_blank" rel="me">
         twitter
       </a>{' '}
       &#8208;{' '}
-      <a href="https://github.com/magarcia" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/magarcia" target="_blank" rel="me">
         github
       </a>{' '}
       &#8208;{' '}
-      <a
-        href="https://stackoverflow.com/users/458193/martin-prins"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href="https://stackoverflow.com/users/458193/martin-prins" target="_blank" rel="me">
         stack overflow
       </a>{' '}
       &#8208;{' '}
-      <a
-        href="https://www.linkedin.com/in/martingarciamonterde/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href="https://www.linkedin.com/in/martingarciamonterde/" target="_blank" rel="me">
         linkedin
       </a>
     </div>
